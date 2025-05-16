fff229161980156e689f0911f429a84f097c1219
Shags001 <dr6496753@gmail.com>
2025-05-16 07:38:45 -0600

    corrigiendo un error gramatical en el titulo

--- Views/MainForm.cs
+++ Views/MainForm.cs
@@ -35,7 +35,7 @@ namespace ZapateriaWinForms.Views
 
             // Encabezado
             var lblTitulo = new Label {
-                Text = "Zapateríag",
+                Text = "Zapatería",
                 Font = new System.Drawing.Font("Segoe UI", 28, System.Drawing.FontStyle.Bold),
                 AutoSize = false,
                 TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
